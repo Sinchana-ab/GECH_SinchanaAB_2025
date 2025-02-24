d7292ad58390b7f9061e8ee90e20a01a2848bb25
Sinchana-ab <sinchanaab.parvam@example.com>
2025-02-24 12:44:16 +0530

    initial commit

---
+++ html/script.js
@@ -0,0 +1,7 @@
+function calculate(){
+    let a = parseInt(document.getElementById("a".value));
+    let b = parseInt(document.getElementById("a".value));  
+    let operator = document.getElementById("a".value);
+    let result = if{
+        
+    }
