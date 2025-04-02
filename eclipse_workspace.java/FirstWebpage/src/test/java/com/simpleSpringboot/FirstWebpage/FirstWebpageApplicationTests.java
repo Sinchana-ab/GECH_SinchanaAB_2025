9921577f98c49975f258e2d68b53658267b89512
Sinchana-ab <sinchanaab.parvam@gmail.com>
2025-04-02 16:05:49 +0530

    Spring Boot

---
+++ eclipse_workspace.java/FirstWebpage/src/test/java/com/simpleSpringboot/FirstWebpage/FirstWebpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleSpringboot.FirstWebpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWebpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
